277b23d52535d1e68019183ea8696381ad8666cd
ayoungminer <39126665+ayoungminer@users.noreply.github.com>
2018-06-06 08:52:53 -0500

    Disabled Timetravel

--- Miners/CcminerTpruvot.ps1
+++ Miners/CcminerTpruvot.ps1
@@ -1,45 +1,44 @@
 . .\Include.ps1
 
-$Path = ".\Bin\NVIDIA-TPruvotcuda9\ccminer.exe"
+$Path = ".\Bin\NVIDIA-TPruvot\ccminer.exe"
 $Uri = "https://github.com/tpruvot/ccminer/releases/download/2.2.5-tpruvot/ccminer-x86-2.2.5-cuda9.7z"
 
 $Commands = [PSCustomObject]@{
-
-    "polytimos" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Polytimos
-    #"hsr" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Hsr
-    #"phi" = " -N 1 -d $SelGPUCC --api-remote --api-allow=0/0" #Phi
-    #"bitcore" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Bitcore
-    "jha" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Jha
-    #"blake2s" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Blake2s
+    "polytimos" = " -d $SelGPUCC --api-remote" #Polytimos
+    #"hsr" = " -d $SelGPUCC --api-remote" #Hsr
+    #"phi" = " -N 1 -d $SelGPUCC --api-remote" #Phi
+    #"bitcore" = " -d $SelGPUCC --api-remote" #Bitcore
+    "jha" = " -d $SelGPUCC --api-remote" #Jha
+    #"blake2s" = " -d $SelGPUCC --api-remote" #Blake2s
     #"blakecoin" = " -d $SelGPUCC" #Blakecoin
     #"vanilla" = "" #BlakeVanilla
     #"cryptonight" = " -i 10 -d $SelGPUCC" #Cryptonight
     #"decred" = "" #Decred
     #"equihash" = "" #Equihash
     #"ethash" = "" #Ethash
-    #"groestl" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Groestl
-    #"hmq1725" = " -d $SelGPUCC -i 20 --api-remote --api-allow=0/0" #hmq1725
-    #"keccakc" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Keccakc
-    "lbry" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Lbry
-    #"lyra2v2" = " -N 1 -d $SelGPUCC --api-remote --api-allow=0/0" #Lyra2RE2
-    #"lyra2z" = "  -d $SelGPUCC --api-remote --api-allow=0/0 --submit-stale" #Lyra2z
+    #"groestl" = " -d $SelGPUCC --api-remote" #Groestl
+    #"hmq1725" = " -d $SelGPUCC -i 20 --api-remote" #hmq1725
+    #"keccakc" = " -d $SelGPUCC --api-remote" #Keccakc
+    "lbry" = " -d $SelGPUCC --api-remote" #Lbry
+    #"lyra2v2" = " -N 1 -d $SelGPUCC --api-remote" #Lyra2RE2
+    "lyra2z" = " -r 0 -d $SelGPUCC --api-remote" #Lyra2z
     #"myr-gr" = "" #MyriadGroestl
     #"neoscrypt" = " -d $SelGPUCC" #NeoScrypt
-    #"nist5" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Nist5
+    #"nist5" = " -d $SelGPUCC --api-remote" #Nist5
     #"pascal" = "" #Pascal
     #"qubit" = "" #Qubit
     #"scrypt" = "" #Scrypt
     "sha256t" = " -d $SelGPUCC" #Sha256t
     #"sia" = "" #Sia
-    #"sib" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Sib
+    #"sib" = " -d $SelGPUCC --api-remote" #Sib
     #"skein" = "" #Skein
     #"skunk" = " -d $SelGPUCC" #Skunk
-    "timetravel" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Timetravel
-    #"tribus" = " -d $SelGPUCC --api-remote --api-allow=0/0" #Tribus
-    #"c11" = " -d $SelGPUCC --api-remote --api-allow=0/0" #C11
+    #"timetravel" = " -d $SelGPUCC --api-remote" #Timetravel
+    #"tribus" = " -d $SelGPUCC --api-remote" #Tribus
+    #"c11" = " -d $SelGPUCC --api-remote" #C11
     #"veltor" = "" #Veltor
-    "x11evo" = " -d $SelGPUCC --api-remote --api-allow=0/0" #X11evo
-    #"x17" = " -N 1 -d $SelGPUCC --api-remote --api-allow=0/0" #X17
+    "x11evo" = " -d $SelGPUCC --api-remote" #X11evo
+    #"x17" = " -N 1 -d $SelGPUCC --api-remote" #X17
     #"yescrypt" = "" #Yescrypt
 }
 
