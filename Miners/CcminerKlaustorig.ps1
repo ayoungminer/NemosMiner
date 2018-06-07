bbdb17210fdedb75799b5c78263834d863844bb2
ayoungminer <39126665+ayoungminer@users.noreply.github.com>
2018-06-07 11:05:27 -0500

    Misc adjustments

--- Miners/CcminerKlaustorig.ps1
+++ Miners/CcminerKlaustorig.ps1
@@ -1,25 +1,25 @@
 . .\Include.ps1
 
-$Path = ".\Bin\NVIDIA-CcminerKlaust\ccminer.exe"
-$Uri = "https://github.com/nemosminer/ccminerKlausT-r11-fix/releases/download/r11-fix/ccminerKlausTr11.7z"
+$Path = ".\Bin\NVIDIA-CcminerKlaustorig\ccminer.exe"
+$Uri = "https://github.com/KlausT/ccminer/releases/download/8.21/ccminer-821-cuda91-x64.zip"
 
 $Commands = [PSCustomObject]@{
     #"bitcore" = "" #Bitcore
     #"blake2s" = "" #Blake2s
-    "blakecoin" = " -r 0 -d $SelGPUCC -q" #Blakecoin
+    #"blakecoin" = " -r 0 -d $SelGPUCC -q" #Blakecoin
     #"vanilla" = "" #BlakeVanilla
     #"cryptonight" = "" #Cryptonight
     #"decred" = "" #Decred
     #"equihash" = "" #Equihash
     #"ethash" = "" #Ethash
-    "groestl" = " -r 0 -d $SelGPUCC" #Groestl
+    #"groestl" = " -r 0 -d $SelGPUCC" #Groestl
     #"hmq1725" = "" #hmq1725
     #"keccak" = " -d $SelGPUCC -q" #Keccak
     #"lbry" = "" #Lbry
     #"lyra2v2" = " -d $SelGPUCC" #Lyra2RE2
     #"lyra2z" = "" #Lyra2z
-    "myr-gr" = " -r 0 -d $SelGPUCC" #MyriadGroestl
-    #"neoscrypt" = " -r 0 -d $SelGPUCC" #NeoScrypt
+    #"myr-gr" = " -r 0 -d $SelGPUCC" #MyriadGroestl
+    "neoscrypt" = " -r 0 -d $SelGPUCC" #NeoScrypt
     #"nist5" = " -d $SelGPUCC" #Nist5
     #"pascal" = "" #Pascal
     #"qubit" = "" #Qubit
@@ -32,10 +32,10 @@ $Commands = [PSCustomObject]@{
     #"veltor" = "" #Veltor
     #"x11evo" = "" #X11evo
     #"c11" = " -d $SelGPUCC" #C11
-    "yescrypt" = " -r 0 -d $SelGPUCC" #yescrypt
-    "yescryptR8" = " -r 0 -d $SelGPUCC"
-    "yescryptR16" = " -r 0 -d $SelGPUCC" #YescryptR16 #Yenten
-    "yescryptR16v2" = " -r 0 -d $SelGPUCC" #PPN
+    #"yescrypt" = " -r 0 -d $SelGPUCC" #yescrypt
+    #"yescryptR8" = " -r 0 -d $SelGPUCC"
+    #"yescryptR16" = " -r 0 -d $SelGPUCC" #YescryptR16 #Yenten
+    #"yescryptR16v2" = " -r 0 -d $SelGPUCC" #PPN
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
