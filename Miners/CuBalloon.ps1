b567d33ccedffa798ea5530ce405bb6553ea9359
ayoungminer <39126665+ayoungminer@users.noreply.github.com>
2018-07-11 07:21:09 -0500

    CuBallon miner adjustment

--- Miners/CuBalloon.ps1
+++ Miners/CuBalloon.ps1
@@ -13,8 +13,8 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -b 4068 -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --cuda_devices 0,1,2,3,4,5,6,7,8,9,10,11 --cuda_threads 64,64,64,64,64,64,64,64,64,64,64,64 --cuda_blocks 48,48,48,48,48,48,48,48,48,48,48,48 --cuda_sync 0 -t 0"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .97} # substract about 3% fee
+        Arguments = " -b 4068 -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --cuda_devices 0,1,2,3,4,5,6,7,8,9,10,11 --cuda_threads 448,448,448,448,448,448,448,448,448,448,448,448 --cuda_blocks 48,48,48,48,48,48,48,48,48,48,48,48 --cuda_sync 0 -t 0"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .9675} # substract about 3% fee
         API = "Ccminer"
         Port = 4068
         Wrap = $false
