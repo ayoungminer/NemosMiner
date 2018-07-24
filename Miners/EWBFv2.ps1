1c49ca101933b987e6d5741bbfefd053fbd0aed3
ayoungminer <39126665+ayoungminer@users.noreply.github.com>
2018-07-24 07:29:07 -0500

    Fix equihash algos

--- Miners/EWBFv2.ps1
+++ Miners/EWBFv2.ps1
@@ -3,10 +3,22 @@
 $Path = ".\Bin\NVIDIA-EWBFv2\\miner.exe"
 $Uri = "https://github.com/ayoungminer/miner-bin/raw/master/EWBFv2/EWBFv2.7z"
 
+# Automatically add Equihash coins if Equihash in algo list
+If ("equihash" -in $Config.Algorithm) {
+	(Get-Content .\Algorithms.txt | ConvertFrom-Json) | Get-Member -MemberType noteproperty | Where {$_.Name -like "equihash*"} | Foreach {
+		If ($_.Name -notin $Config.Algorithm) {
+			$Config.Algorithm += $_.Name
+		}
+	}
+}
+
 $Commands = [PSCustomObject]@{
     "equihash144" = " --cuda_devices $SelGPUDSTM --algo 144_5 --pers sngemPoW" #Equihash144
-    "equihash192" = " --cuda_devices $SelGPUDSTM --algo 192_7 --pers ZERO_PoW" #Equihash192
     "equihash144btcz" = " --cuda_devices $SelGPUDSTM --algo 144_5 --pers BitcoinZ" #Equihash144btcz
+    "equihash144zel" = " --cuda_devices $SelGPUDSTM --algo 144_5 --pers ZelProof" #Equihash144zel
+    "equihash192" = " --cuda_devices $SelGPUDSTM --algo 192_7 --pers ZERO_PoW" #Equihash192
+    "equihash-btg" = "--cuda_devices $SelGPUDSTM --algo 144_5 --pers BgoldPoW" # Equihash-btg
+
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -15,12 +27,15 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "--api 127.0.0.1:4068 --server $($Pools.(Get-Algorithm($_)).Host) --port $($Pools.(Get-Algorithm($_)).Port) --fee 0 --eexit 1 --user $($Pools.(Get-Algorithm($_)).User) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --intensity 64"
+        Arguments = "--templimit 95 --api 127.0.0.1:4068 --server $($Pools.(Get-Algorithm($_)).Host) --port $($Pools.(Get-Algorithm($_)).Port) --fee 0 --eexit 1 --user $($Pools.(Get-Algorithm($_)).User) --pass $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) --intensity 64"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day}
         API = "EWBF"
         Port = 4068
         Wrap = $false
         URI = $Uri
+        User = $Pools.(Get-Algorithm($_)).User
+        Host = $Pools.(Get-Algorithm $_).Host
+        Coin = $Pools.(Get-Algorithm $_).Coin
     
     }
 }
