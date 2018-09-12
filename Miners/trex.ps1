dfa5a99cfdf7d9016eec980ebb0829dc6d49a60c
ayoungminer <39126665+ayoungminer@users.noreply.github.com>
2018-09-12 08:18:58 -0500

    Add algos for trex miner

--- Miners/trex.ps1
+++ Miners/trex.ps1
@@ -4,10 +4,14 @@ $Path = ".\Bin\NVIDIA-trex\t-rex.exe"
 $Uri = "http://nemos.dx.am/opt/nemos/t-rex063.7z"
 
 $Commands = [PSCustomObject]@{
+    "balloon" = "" #Balloon
+    "bitcore" = "" #Bitcore
     "c11" = "" #C11
     "hsr" = "" #Hsr
-    "phi"= "" #Phi
     "lyra2z" = "" #Lyra2z
+    "phi"= "" #Phi
+    "polytimos" = "" #Poly
+    "skunk" = "" #Skunk
     "sonoa" = "" #SonoA
     "tribus" = "" #Tribus
     "x17" = "" #X17
@@ -21,7 +25,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = "-b 127.0.0.1:4068 -d $SelGPUCC -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_) -r 5"
+        Arguments = "-b 127.0.0.1:4068 -d $SelGPUCC -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
         API = "Ccminer"
         Port = 4068
