460eb02bd078b97d34d4ad9699e9ae31f305361f
ayoungminer <39126665+ayoungminer@users.noreply.github.com>
2019-05-24 10:23:48 -0500

    Update Gminer.ps1

--- Miners/Gminer.ps1
+++ Miners/Gminer.ps1
@@ -1,7 +1,7 @@
 . .\Include.ps1
  
 $Path = ".\Bin\NVIDIA-Gminer\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.43/gminer_1_43_windows64.zip"
+$Uri = "https://github.com/RainbowMiner/miner-binaries/releases/download/v1.44-gminer/gminer_1_44_windows64.zip"
 
 $Commands = [PSCustomObject]@{
     "beam"         = " --devices $SelGPUDSTM --algo 150_5 --pers auto" #Equihash150
